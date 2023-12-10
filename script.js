b7f82322f0e0f31e664e0dd0c15af3c1a7beeff7
keyur7758 <keyurk397@gmail.com>
2023-12-10 16:52:38 +0530

    added files

---
+++ script.js
@@ -0,0 +1,33 @@
+$(".cl-mode").click(function () {
+    $(".cl-mode").attr("style", "color:white;")
+    $(".gg").attr("style", "color:black;")
+    $(".body-1").attr("style", "background:black;transition:all ease 0.3s;")
+    $(".leftsection").attr("style", "color:white;")
+    $(".btn").attr("style", "border: 1px solid white;color: white;")
+    $(".information .br").attr("style", "border-top: 1px solid #1f1e1e;border-right: 1px solid #1f1e1e;")
+    $(".navbar-area-2 h1").attr("style", "color:black;")
+    $(".thin-line h4").attr("style", "color:black;")
+    $(".gallery-1 .box h4").attr("style", "color:black;")
+    $(".gallery-1 .box h6").attr("style", "color:black;")
+    $(".second-head h1").attr("style", "color:white;")
+
+})
+
+$(".cl-mode").dblclick(function () {
+    $(".body-1").attr("style", "transition:all ease 0.3s;")
+    $(".ag-courses-item_link").attr("style", "")
+    $(".ag-courses-item_title").attr("style", "")
+    $(".btn").attr("style", "")
+    $(".leftsection").attr("style", "")
+    $(".cl-mode").attr("style", "")
+    $(".gg").attr("style", "")
+    $(".information .br").attr("style", "")
+    $(".navbar-area-2 h1").attr("style", "")
+    $(".thin-line h4").attr("style", "")
+    $(".gallery-1 .box h4").attr("style", "")
+    $(".gallery-1 .box h6").attr("style", "")
+    $(".second-head h1").attr("style", "")
+
+})
+
+// rgb(179, 106, 248)
