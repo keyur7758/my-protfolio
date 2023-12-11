9892fd229a2620eb2621381ccbac49d39af46d9b
keyur7758 <keyurk397@gmail.com>
2023-12-11 22:43:42 +0530

    change some images

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-$(".cl-mode").click(function () {
-    $(".cl-mode").attr("style", "color:white;")
-    $(".gg").attr("style", "color:black;")
-    $(".body-1").attr("style", "background:black;transition:all ease 0.3s;")
-    $(".leftsection").attr("style", "color:white;")
-    $(".btn").attr("style", "border: 1px solid white;color: white;")
-    $(".information .br").attr("style", "border-top: 1px solid #1f1e1e;border-right: 1px solid #1f1e1e;")
-    $(".navbar-area-2 h1").attr("style", "color:black;")
-    $(".thin-line h4").attr("style", "color:black;")
-    $(".gallery-1 .box h4").attr("style", "color:black;")
-    $(".gallery-1 .box h6").attr("style", "color:black;")
-    $(".second-head h1").attr("style", "color:white;")
+var isDarkMode = false;
 
-})
+$(".cl-mode").click(function () {
+    isDarkMode = !isDarkMode; // Toggle the mode
+    updateStyles();
+});
 
 $(".cl-mode").dblclick(function () {
-    $(".body-1").attr("style", "transition:all ease 0.3s;")
-    $(".ag-courses-item_link").attr("style", "")
-    $(".ag-courses-item_title").attr("style", "")
-    $(".btn").attr("style", "")
-    $(".leftsection").attr("style", "")
-    $(".cl-mode").attr("style", "")
-    $(".gg").attr("style", "")
-    $(".information .br").attr("style", "")
-    $(".navbar-area-2 h1").attr("style", "")
-    $(".thin-line h4").attr("style", "")
-    $(".gallery-1 .box h4").attr("style", "")
-    $(".gallery-1 .box h6").attr("style", "")
-    $(".second-head h1").attr("style", "")
+    isDarkMode = false; // Reset to default mode
+    updateStyles();
+});
 
-})
+function updateStyles() {
+    var backgroundColor = isDarkMode ? "black" : ""; // Set background color based on mode
+    var textColor = isDarkMode ? "white" : "black"; // Set text color based on mode
+    var borderColor = isDarkMode ? "#1f1e1e" : ""; // Set border color based on mode
 
-// rgb(179, 106, 248)
+    $(".cl-mode").attr("style", "color:" + textColor + ";");
+    $(".gg").attr("style", "color:" + textColor + ";");
+    $(".body-1").attr("style", "background:" + backgroundColor + ";transition:all ease 0.3s;");
+    $(".leftsection").attr("style", "color:" + textColor + ";");
+    $(".btn").attr("style", "border: 1px solid " + textColor + ";color: " + textColor + ";");
+    $(".information .br").attr("style", "border-top: 1px solid " + borderColor + ";border-right: 1px solid " + borderColor + ";");
+    $(".navbar-area-2 h1").attr("style", "color:" + textColor + ";");
+    $(".thin-line h4").attr("style", "color:" + textColor + ";");
+    $(".gallery-1 .box h4").attr("style", "color:" + textColor + ";");
+    $(".gallery-1 .box h6").attr("style", "color:" + textColor + ";");
+    $(".second-head h1").attr("style", "color:" + textColor + ";");
+}
